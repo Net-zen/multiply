b74af0c97c1c8389fd26f0528d9fe372b59c2b85
Net-zen <prankster@myttk.ru>
2019-10-01 18:10:54 +0600

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  const a = first.split('').reverse();
+  const b = second.split('').reverse();
+  const product = [];
+  a.forEach((el, i) => {
+    b.forEach((e, j) => {
+      product.splice((i+j),1 ,((product[i+j]) 
+                                ? product[i+j] + (el * e) 
+                                : (el * e))
+      );
+    });
+  });
+  product.forEach((element, i) => {
+    product.splice((i),1, product[i] %= 10);
+    let stack = Math.floor(element / 10);
+    if (product[i+1]){
+      product.splice((i+1),1, product[i+1] += stack);
+    } else if (stack){
+      product.push(stack);
+    }
+  });
+  return product.reverse().join('');
 }
